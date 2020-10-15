2c21d1b2ba6b9721f21045a05578ac4a569814df
levy pan <levy-pan@outlook.com>
2020-10-15 19:52:31 +0800

    update on 2020-10-15

--- learn-iOS-FoodTracker/learn-iOS-FoodTracker/MealTableViewController.swift
+++ learn-iOS-FoodTracker/learn-iOS-FoodTracker/MealTableViewController.swift
@@ -11,56 +11,64 @@ class MealTableViewController: UITableViewController {
 
 	//MARK: Properties
 	 
-	var meals = [Meal]()
-	
-	
+	var meals = [Meal] ()
+
 	// MARK: Private Methods
 	private func loadSampleMeals(){
 		let photo1 = UIImage(named: "meal1")
 		let photo2 = UIImage(named: "meal2")
 		let photo3 = UIImage(named: "meal3")
 		
-		let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4)
-		let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5)
-		let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 5)
-		
-		guard meal1 != nil else {
+		guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
 			fatalError("Unable to instantiate meal1")
 		}
-		
-		guard meal2 != nil else {
+		 
+		guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
+			fatalError("Unable to instantiate meal2")
+		}
+		 
+		guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3) else {
 			fatalError("Unable to instantiate meal2")
 		}
 		
-		guard meal3 != nil else {
-			fatalError("Unable to instantiate meal3")
-		}
-		
-		// MARK: unfinished: add the meal to the meals
-		
+		meals += [meal1, meal2, meal3]
 	}
 	
 
 	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+		
+		// Load the sample data.
+		loadSampleMeals()
     }
 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+	
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+		return meals.count
     }
 
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+	
+		// Table view cells are reused and should be dequeued using a cell identifier.
+		let cellIdentifier = "MealTableViewCell"
+		
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+			fatalError("The dequeued cell is not an instance of MealTableViewCell. Error in tableView()")
+		}
+		
+		// Fetches the appropriate meal for the data source layout.
+		let meal = meals[indexPath.row]
+		
+		// Configure the cell
+		cell.nameLabel.text = meal.name
+		cell.photoImageView.image = meal.photo
+		cell.ratingControl.rating = meal.rating
+
+		return cell
+	}
    
 }
