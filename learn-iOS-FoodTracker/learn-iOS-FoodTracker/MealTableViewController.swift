9e13d6d82059d13879747776079c162d85224bf0
levy pan <levy-pan@outlook.com>
2020-10-17 09:16:21 +0800

    finished

--- learn-iOS-FoodTracker/learn-iOS-FoodTracker/MealTableViewController.swift
+++ learn-iOS-FoodTracker/learn-iOS-FoodTracker/MealTableViewController.swift
@@ -6,13 +6,38 @@
 //
 
 import UIKit
+import os.log
 
 class MealTableViewController: UITableViewController {
-
-	//MARK: Properties
+	
+	// MARK: Properties
 	 
 	var meals = [Meal] ()
+	// MARK: Actions
+	@IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+		if let sourceViewController = sender.source as? MealViewController ,let meal = sourceViewController.meal{
+			
+			if let selectedIndexPath = tableView.indexPathForSelectedRow {
+				// Update an existing meal.
+				meals[selectedIndexPath.row] = meal
+				tableView.reloadRows(at: [selectedIndexPath], with: .none)
+			}else{
+				// Save the meals.
+				saveMeals()
+				
+				// Add a new meal.
+				let newIndexPath = IndexPath(row: meals.count, section: 0)
+				
+				meals.append(meal)
+				tableView.insertRows(at: [newIndexPath], with: .automatic)
+			}
+			
+			// Save the meals.
+			saveMeals()
 
+		}
+	}
+	
 	// MARK: Private Methods
 	private func loadSampleMeals(){
 		let photo1 = UIImage(named: "meal1")
@@ -34,12 +59,33 @@ class MealTableViewController: UITableViewController {
 		meals += [meal1, meal2, meal3]
 	}
 	
+	private func saveMeals(){
+		let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+		
+		if isSuccessfulSave {
+			os_log("Meals successfully saved.", log: OSLog.default, type:.debug)
+		}else{
+			os_log("Failed to save meals...", log: OSLog.default, type:.error)
 
+		}
+	}
+	
+	// MARK: viewDidLoad
 	override func viewDidLoad() {
         super.viewDidLoad()
 		
-		// Load the sample data.
-		loadSampleMeals()
+		// Use the edit button item provided by the table view controller
+		navigationItem.leftBarButtonItem = editButtonItem
+		
+		// Load any saved meals, otherwise load sample data.
+		if let savedMeals = loadMeals() {
+			meals += savedMeals
+		}
+		else {
+			// Load the sample data.
+			loadSampleMeals()
+		}
+		
     }
 
     // MARK: - Table view data source
@@ -70,5 +116,59 @@ class MealTableViewController: UITableViewController {
 
 		return cell
 	}
+	
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			// Delete the row from the data source
+			meals.remove(at: indexPath.row)
+			saveMeals()
+			tableView.deleteRows(at: [indexPath], with: .fade)
+		} else if editingStyle == .insert {
+			// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+		}
+	}
+	
+	override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+		// Return false if you do not want the specified item to be editable.
+		return true
+	}
+	
+	
+	//MARK: - Navigation
+	 
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		
+		super.prepare(for: segue, sender: sender)
+		
+		switch (segue.identifier ?? "") {
+
+		case "AddItem":
+		os_log("Adding a new meal.", log: OSLog.default, type: .debug)
+
+		case "ShowDetail":
+			guard let mealDetailViewController = segue.destination as? MealViewController else {
+				fatalError("Unexpected destination: \(segue.destination)")
+			}
+			
+			guard let selectedMealCell = sender as? MealTableViewCell else {
+				fatalError("Unexpected sender: \(String(describing: sender))")
+			}
+			
+			guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
+				fatalError("The selected call is not being displayed by the table")
+			}
+			
+			let selectedMeal = meals[indexPath.row]
+			mealDetailViewController.meal = selectedMeal
+		default:
+			fatalError("Unexpected Segue Identifier; \(String(describing: segue.identifier))")
+		}
+	}
+	
+	private func loadMeals() -> [Meal]?{
+		return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
+	}
+	
    
 }
--- learn-iOS-FoodTracker/learn-iOS-FoodTracker/MealViewController.swift
+++ learn-iOS-FoodTracker/learn-iOS-FoodTracker/MealViewController.swift
@@ -6,18 +6,52 @@
 //
 
 import UIKit
+import os.log
 
 class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
 	
 	// MARK: Properties
 	@IBOutlet weak var nameTextField: UITextField!
 	@IBOutlet weak var photoImageView: UIImageView!
 	@IBOutlet weak var ratingControl: RatingControl!
+	@IBOutlet weak var saveButton: UIBarButtonItem!
+	@IBAction func cancel(_ sender: Any) {
+		// Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways
+		let isPresentingInAddMealMode = presentingViewController is UINavigationController
+		
+		if isPresentingInAddMealMode {
+			dismiss(animated: true, completion: nil)
+		} else if let owningNavigationController = navigationController {
+			owningNavigationController.popViewController(animated: true)
+		}else{
+			fatalError("The MealViewController is not inside a navigation controller.")
+		}
+	}
+	
+	/*
+	 This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+	 or constructed as part of adding a new meal.
+	 */
+	var meal: Meal?
+	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
 		// Do any additional setup after loading the view.
 		nameTextField.delegate = self
+		
+		// Set up views if editing an existing Meal.
+		if let meal = meal {
+			navigationItem.title = meal.name
+			nameTextField.text = meal.name
+			photoImageView.image = meal.photo
+			ratingControl.rating = meal.rating
+		}
+		
+		// Enable the Save button only if the text field has a valid Meal name.
+		updateSaveButtonState()
 	}
 	
 	//MARK: UITextFieldDelegate
@@ -27,9 +61,17 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
 		return true
 	}
 	
-	func textFieldDidEndEditing(_ textField: UITextField) {
-
+	func textFieldDidBeginEditing(_ textField: UITextField) {
+		// Disable the Save button while editing
+		saveButton.isEnabled = false
 	}
+	
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		updateSaveButtonState()
+		navigationItem.title = textField.text
+	}
+	
+	
 	// MARK: UIImagePickerControllerDelegate
 	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
 		// Dismiss the picker if user canceld
@@ -51,7 +93,24 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
 		dismiss(animated: true, completion: nil)
 	}
 	
-	
+	// MARK: Navigation
+	// This method lets you configure a view controller before it's presented.
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		super.prepare(for: segue, sender: sender)
+		
+		// Configure the destination view controller only when the save button is pressed.
+		guard let button = sender as? UIBarButtonItem, button == saveButton else {
+			os_log("This save button was not pressed, cancelling ", log: .default, type:.debug)
+			return
+		}
+		
+		let name = nameTextField.text ?? ""
+		let photo = photoImageView.image
+		let rating = ratingControl.rating
+		
+		// Set the meal to be passed to MealTableViewController after the unwind segue.
+		meal = Meal(name: name, photo: photo, rating: rating)
+	}
 	
 	// MARK: Action
 	@IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
@@ -70,5 +129,10 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
 		
 	}
 	
-	
+	// MARK: Private Methods
+	private func updateSaveButtonState() {
+		// Disable the Save button if the text field is empty
+		let text = nameTextField.text ?? ""
+		saveButton.isEnabled = !text.isEmpty
+	}
 }
